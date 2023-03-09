7b58a4a1004bf7051dde3ac59381d83e23fe2a70
ArtisticCloud <101282869+ArtisticCloud@users.noreply.github.com>
2023-03-08 22:05:50 -0500

    e

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,3 @@
-repeat task.wait() until game:IsLoaded()
 local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Linoria = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
