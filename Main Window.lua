4b20b0a9233d84605e0b6593e210e7bab92ef9f2
ArtisticCloud <101282869+ArtisticCloud@users.noreply.github.com>
2023-03-08 20:38:41 -0500

    e

--- Main Window.lua
+++ Main Window.lua
@@ -2,13 +2,13 @@ local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Linoria = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
-local init = require(init)  
-print( 'init' , init ) 
-
 --// Create the window //--
-Linoria:CreateWindow({
-    Title = "Art's Boosting Hub" , 
-    AutoShow = true , 
-})
+function Run()
+    Linoria:CreateWindow({
+        Title = "Art's Boosting Hub" , 
+        AutoShow = true , 
+    })
+end 
 
+Run() ; 
 --// tabs //--
--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Linoria = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+
+print( 'module running' )
