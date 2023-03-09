d23bc7cd7de49d36dc70d67624879e2dc0270d1e
ArtisticCloud <101282869+ArtisticCloud@users.noreply.github.com>
2023-03-08 20:12:42 -0500

    starting

---
+++ Main Window.lua
@@ -0,0 +1,4 @@
+local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
+local Linoria = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
