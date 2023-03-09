f3afa3a4f2a7b85b88a3bf271ed95557f240cdc8
ArtisticCloud <101282869+ArtisticCloud@users.noreply.github.com>
2023-03-09 18:07:19 -0500

    e

--- MainScript.lua
+++ MainScript.lua
@@ -1,3 +1,5 @@
+repeat task.wait() until getgenv().Utility
+print( "Art's Hub Debug: | Modules Loaded" )
 local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Linoria = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
