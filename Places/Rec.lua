017aa354a6860648435f1be10746f7a03d89bf22
ArtisticCloud <101282869+ArtisticCloud@users.noreply.github.com>
2023-03-14 00:21:28 -0400

    e

--- Places/Rec.lua
+++ Places/Rec.lua
@@ -156,7 +156,7 @@ function Rec:createPartyCodes( OtherMain )
             return
         end
     elseif not Remotes:FindFirstChild( 'Parties' ) then
-        self.Linoria:Notify( 'bro your not even in rec' , 8  )
+        self.Linoria:Notify( 'bro your not even in rec' , 8.5  )
     end
 end     
 
